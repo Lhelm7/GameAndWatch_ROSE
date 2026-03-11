555e720a7c4ec2d2857f50825739d76b197247a3
Lhelm7 <roroleroy84@gmail.com>
2026-03-11 19:47:42 +0100

    [RRO]

--- Assets/Games/Game2/Script/GameOver2.cs
+++ Assets/Games/Game2/Script/GameOver2.cs
@@ -4,11 +4,13 @@ public class GameOver2 : MonoBehaviour
 {
     public void RetryGame(string sceneName)
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("2ndGame");
     }
     
     public void QuitGame(string sceneName)
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("MenuChangeGame");
     }
 }
